89fe84dfda411f9855a6aea2de3e61434e5d0d85
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-07-19 23:52:46 -0300

    novos operadores

---
+++ operators/operadores de atribuição/ex.py
@@ -0,0 +1,22 @@
+a = 10
+a += 5
+print(a)
+
+b = 30
+b -= 20
+print(b)
+
+c = 2
+c *=2
+print(c)
+
+d = 10
+d /=10
+print(d)
+
+
+e = 1
+e %=1
+print(e)
+
+
---
+++ operators/operadores de identidade/main.py
@@ -0,0 +1,7 @@
+string_01 = "ola mundão"
+string_02 = "ola mundão"
+
+print(type(string_01) == type(string_02))
+
+print(string_01 is string_02)
+print(string_01 is not  string_02)
